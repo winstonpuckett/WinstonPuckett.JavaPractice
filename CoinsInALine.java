d24307a0c5d6446c334817cd2f8567e411a54b06
Winston Puckett <puckett.bobby7@gmail.com>
2021-07-21 21:09:33 -0700

    Working solution

--- CoinsInALine.java
+++ CoinsInALine.java
@@ -1,4 +1,5 @@
 /*
+
     Source: https://www.interviewbit.com/problems/coins-in-a-line/
     Description:
     There are A coins (Assume A is even) in a line.
@@ -18,6 +19,37 @@ public class CoinsInALine {
     }
 
     private static int calculateMaxCoins(int[] coins) {
-        return 0;
+        var playerOneScore = 0;
+        var playerTwoScore = 0;
+        var isPlayerOnesTurn = true;
+
+        while (coins.length > 0) {
+            var tempCoinsArray = new int[coins.length - 1];
+            if (isPlayerOnesTurn) {
+                if (coins[0] > coins[coins.length - 1]) {
+                    playerOneScore += coins[0];
+                    System.arraycopy(coins, 1, tempCoinsArray, 0, coins.length - 1);
+                    coins = tempCoinsArray;
+                } else {
+                    playerOneScore += coins[coins.length - 1];
+                    System.arraycopy(coins, 0, tempCoinsArray, 0, coins.length - 1);
+                    coins = tempCoinsArray;
+                }
+            } else {
+                if (coins[0] > coins[coins.length - 1]) {
+                    playerTwoScore += coins[0];
+                    System.arraycopy(coins, 1, tempCoinsArray, 0, coins.length - 1);
+                    coins = tempCoinsArray;
+                } else {
+                    playerTwoScore += coins[coins.length - 1];
+                    System.arraycopy(coins, 0, tempCoinsArray, 0, coins.length - 1);
+                    coins = tempCoinsArray;
+                }
+            }
+
+            isPlayerOnesTurn = !isPlayerOnesTurn;
+        }
+
+        return playerOneScore;
     }
 }
